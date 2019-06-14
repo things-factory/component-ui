8294786a2d47ad4f16f3f65db944efe47240f44f
leemyunggyu <jaylee@hatiolab.com>
2019-06-14 16:51:37 +0800

    v0.0.15

--- component/popup/pop-up.js
+++ component/popup/pop-up.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit-element'
+import '@material/mwc-button/mwc-button'
 
 class PopUp extends LitElement {
   static get styles() {
@@ -10,23 +11,59 @@ class PopUp extends LitElement {
         top: 0;
         bottom: 0;
         background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        flex-direction: column;
+        padding: 10vw;
+        z-index: 1;
+        display: none;
+      }
+      #header {
+        display: flex;
+        margin: 10px 10px 0 10px;
+      }
+      #header > .title {
+        margin: 0;
+        flex: 1;
+      }
+      #container {
+        max-height: calc(100% - 40px);
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        border-radius: 5px;
+        background-color: #fff;
+      }
+      #content {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        overflow: hidden;
+        margin: 10px;
       }
     `
   }
 
   static get properties() {
     return {
-      open: Boolean,
+      title: String,
+      opened: Boolean,
       openCallback: Object,
       closeCallback: Object
     }
+  }
+
+  constructor() {
+    super()
+    this.opened = false
+    this.addEventListener('click', this.close.bind(this))
   }
 
   render() {
     return html`
       <div id="container">
         <div id="header">
-          <h2>${this.title}</h2>
+          <h2 class="title">${this.title}</h2>
+          <mwc-button @click="${this.close}">X</mwc-button>
         </div>
 
         <div id="content">
@@ -37,8 +74,10 @@ class PopUp extends LitElement {
   }
 
   open() {
-    this.open = true
-    this.dispatchEvent('open')
+    this.dispatchEvent(new CustomEvent('open'))
+
+    this.style.display = 'block'
+    this.opened = true
 
     if (this.openCallback && typeof this.openCallback === 'function') {
       this.openCallback()
@@ -46,8 +85,10 @@ class PopUp extends LitElement {
   }
 
   close() {
-    this.open = false
-    this.dispatchEvent('close')
+    this.dispatchEvent(new CustomEvent('close'))
+
+    this.style.display = 'none'
+    this.opened = false
 
     if (this.closeCallback && typeof this.closeCallback === 'function') {
       this.closeCallback()
@@ -55,7 +96,7 @@ class PopUp extends LitElement {
   }
 
   toggle() {
-    if (this.open) {
+    if (this.opened) {
       this.close()
     } else {
       this.open()
